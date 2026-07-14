b52dd2e644d9e9a2a642b5de90d485bc1e13d933
sivaprasad2848 <sivaprasad@cybersquare.org>
2026-07-14 15:36:03 +0530

    Menu Added

--- contact.py
+++ contact.py
@@ -1,10 +1,18 @@
 y=0
 contacts=[]
 while y==0:
-    name=input("Enter Name")
-    email=input("Enter Email")
-    mobile=input("Enter Mobile")
-    #print(name+" "+email+" "+mobile)
-    contacts.append((name,email,mobile))
-    print(contacts)
+    print("---Menu----")
+    print("1->For insert contact")
+    print("2->For display contact")
+    print("3->For delete contact")
+    print("4->For update contact")
+    opt=int(input("Enter your option"))
+    if(opt==1):
+        name=input("Enter Name")
+        email=input("Enter Email")
+        mobile=input("Enter Mobile")
+        #print(name+" "+email+" "+mobile)
+        contacts.append((name,email,mobile))
+    if opt==2:
+        print(contacts)
     y=int(input("Do You wnat to continue? press 0 for yes"))
