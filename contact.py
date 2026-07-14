05037819bd9d378b13e82899a31a67cd225516c3
sivaprasad2848 <sivaprasad@cybersquare.org>
2026-07-14 15:32:45 +0530

    Variables,Loops,List,Tuple

--- contact.py
+++ contact.py
@@ -1,4 +1,10 @@
-name=input("Enter Name")
-email=input("Enter Email")
-mobile=input("Enter Mobile")
-print(name+" "+email+" "+mobile)
+y=0
+contacts=[]
+while y==0:
+    name=input("Enter Name")
+    email=input("Enter Email")
+    mobile=input("Enter Mobile")
+    #print(name+" "+email+" "+mobile)
+    contacts.append((name,email,mobile))
+    print(contacts)
+    y=int(input("Do You wnat to continue? press 0 for yes"))
